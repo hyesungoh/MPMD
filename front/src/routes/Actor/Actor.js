c2dc4c8bfe091041afba736730dffacabbfbe4a9
hyesungoh <haesungoh414@gmail.com>
2020-12-08 21:26:59 +0900

    add axios in actor route

--- front/src/routes/Actor/Actor.js
+++ front/src/routes/Actor/Actor.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Actor = () => {
+    // status state 0을 기본으로 생성 
+    // > 0이면 get, 1이면 post로 데이터 가져온 후 셋스테이트
+    // 
+    const [actorData, setActorData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+
+    const useActorData = () => {
+        const fetchUrl = "http://127.0.0.1:8000/api/actor";
+        axios.get(fetchUrl).then(
+            (response) => {
+                setActorData(response.data);
+                setIsLoaded(true);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        );
+    };
+
+    useEffect(useActorData, []);
+
+    const CheckStatus = () =>
+        actorData.status == 0 ? (
+
+            <div>
+                <h1>this is actor get</h1>
+                <form method="POST">
+                    <input type="text" name={actorData.form_data.month} />
+                    <input type="text" name={actorData.form_data.day} />
+                    <input type="submit" value="submit" />
+                </form>
+            </div>
+        ) : (
+            <div></div>
+        );
+
     return (
         <div>
-            <h1>actor</h1>
+            {isLoaded ? (
+                <CheckStatus />
+            ) : (
+                <div>
+                    <h1>hellog</h1>
+                </div>
+            )}
         </div>
     );
 };
