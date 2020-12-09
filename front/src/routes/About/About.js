c29ebaf0f0162bd3e45197c597fd0b5e8a2f42c1
hyesungoh <haesungoh414@gmail.com>
2020-12-10 01:57:24 +0900

    design about component

--- front/src/routes/About/About.js
+++ front/src/routes/About/About.js
@@ -4,8 +4,14 @@ import "./About.css";
 
 const About = () => {
     return (
-        <div>
-            <h1>about</h1>
+        <div className="about">
+            <div className="about__card">
+                <h3>Oh Hyesung</h3>
+                <div>
+                    <h5>github.com/hyesungoh</h5>
+                    <h5>haesungoh414@gmail.com</h5>
+                </div>
+            </div>
         </div>
     );
 };
