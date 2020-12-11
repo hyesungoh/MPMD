7b3fe5ec067e7a6b4d70fbfb0f0a08eced84d859
hyesungoh <haesungoh414@gmail.com>
2020-12-11 23:15:59 +0900

    add 3d animation with react spring

--- front/src/routes/Actor/ActorCard/ActorCard.js
+++ front/src/routes/Actor/ActorCard/ActorCard.js
@@ -1,22 +1,57 @@
 import React from "react";
-import Actor from "../Actor";
+import { useSpring, animated } from "react-spring";
+
+const calc = (x, y, s) => [
+    -(y - window.innerHeight / 2) / 30,
+    (x - window.innerWidth / 2) / 30,
+    s,
+];
+const trans = (x, y, s) =>
+    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
+const trans_img = (x, y, s) =>
+    `perspective(300px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 const ActorCard = (props) => {
-    console.log(props);
     const { name, image_src, image_href, summary } = props.props;
     const image_base_url = "https://www.imdb.com";
+
+    const [pr, set] = useSpring(() => ({
+        xys: [0, 0, 1],
+        config: { mass: 5, tension: 350, friction: 40 },
+    }));
+
+    const handleClick = (href) => {
+        window.open(image_base_url + href);
+        console.log(href);
+    };
+
     return (
-        <div className="card">
-            <div className="card__img">
+        <animated.div
+            className="card"
+            onMouseMove={({ clientX: x, clientY: y }) =>
+                set({ xys: calc(x, y, 1.1) })
+            }
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{ transform: pr.xys.interpolate(trans) }}
+            onClick={() => {
+                handleClick(image_href);
+            }}
+        >
+            <animated.div
+                className="card__img"
+                style={{ transform: pr.xys.interpolate(trans_img) }}
+            >
                 <img src={image_src} alt={name} />
-            </div>
+            </animated.div>
+
             <div className="card__name">
                 <span>{name}</span>
             </div>
             <div className="card__summary">
                 <span>{summary}</span>
             </div>
-        </div>
+        </animated.div>
     );
 };
 
